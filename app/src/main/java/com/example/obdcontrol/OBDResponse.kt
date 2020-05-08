8a3d2b8ca3175b41b86b9c547d20e57fa767f5f4
SHIMIZU, Kenji <sken2@yahoo.co.jp>
2020-05-08 09:58:36 +0900

    implement OBDresponse

--- app/src/main/java/com/example/obdcontrol/OBDResponse.kt
+++ app/src/main/java/com/example/obdcontrol/OBDResponse.kt
@@ -1,10 +1,39 @@
 package com.example.obdcontrol
 
-class OBDResponse(data : String) : OBDRequest(data) {
-    var isValid : Boolean = true
-    private val org = data
+import kotlin.properties.Delegates
+
+class OBDResponse(data : String, hasHeader : Boolean) {
+    private var valid = false
+    private var codes = emptyArray<Byte>()
+    private val hasHeader = hasHeader
+
+    init {
+        var firstDigit = true
+        var hexValue = 0
+        data.forEach { chr ->
+            val hex = Character.digit(chr, 16)
+            if (hex != -1) {
+                if (firstDigit) {
+                    hexValue = hex *16
+                } else {
+                    codes += (hexValue + hex).toByte()
+                    hexValue = 0
+                }
+                firstDigit = !firstDigit
+            }   // TODO checkout non-hexadecimal char
+        }
+        if (codes.isNotEmpty() and firstDigit) {
+            valid = true
+        }
+    }
+
+    fun isValid() : Boolean {
+        return valid
+    }
 
     override fun toString() : String {
-        return org  // TODO
+        var stringValue = ""
+        codes.forEach { stringValue += "%02X ".format(it) }
+        return stringValue.trim()
     }
 }
