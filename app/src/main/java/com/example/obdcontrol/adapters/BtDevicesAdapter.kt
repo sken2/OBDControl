d3ff14b61dc06efd4ca90b8d9f4e12ef3112daf1
SHIMIZU, Kenji <sken2@yahoo.co.jp>
2020-06-26 17:02:34 +0900

    implement device search fragment and adapter

---
+++ app/src/main/java/com/example/obdcontrol/adapters/BtDevicesAdapter.kt
@@ -0,0 +1,72 @@
+package com.example.obdcontrol.adapters
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.os.ParcelUuid
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.obdcontrol.Const
+import com.example.obdcontrol.R
+import java.util.*
+
+class BtDevicesAdapter : RecyclerView.Adapter<BtDevicesAdapter.ViewHolder>() {
+
+    val list = mutableListOf<BluetoothDevice>()
+    var selected = -1
+    var matchOnlySppDevice = false
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        Log.v(Const.TAG, "BtDeviceAdapter::onAttachedToRecyclerView")
+        refresh()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        Log.v(Const.TAG, "BleDeviceAdapter::onCreateViewHolder")
+        val contentView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_bluetooth_device, parent, false)
+        return ViewHolder(contentView)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        Log.v(Const.TAG, "BtDeviceAdapter::onBindViewHolder")
+        holder.name.text = list.get(position).name
+        holder.select.setOnClickListener{
+            if (position != selected) {
+                Log.v(Const.TAG, "BtDeviceAdapter::onClickListener position = $position")
+                selected = position
+            } else {
+                // reset color
+                selected = -1
+            }
+        }
+        return
+    }
+
+    fun refresh() {
+        val adapter = BluetoothAdapter.getDefaultAdapter()
+        adapter?.run {
+            list.clear()
+            val newList = adapter.bondedDevices
+                .filter {
+                    !matchOnlySppDevice and
+                    it.uuids.contains(ParcelUuid(UUID.fromString(Const.UUIDS.SPP_UUID)))
+                }
+            list.addAll(newList)
+        }
+    }
+
+    inner class ViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        val name = view.findViewById<TextView>(R.id.text_idem_device_name)
+        val select = view.findViewById<ImageButton>(R.id.button_item_select)
+    }
+}
--- app/src/main/java/com/example/obdcontrol/ui/DeviceSearchFragment.kt
+++ app/src/main/java/com/example/obdcontrol/ui/DeviceSearchFragment.kt
@@ -1,23 +1,62 @@
 package com.example.obdcontrol.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.obdcontrol.Const
+import com.example.obdcontrol.R
+import com.example.obdcontrol.adapters.BtDevicesAdapter
 
 class DeviceSearchFragment : Fragment() {
+
+    private val adapter by lazy {
+        BtDevicesAdapter()
+    }
+    private val preference by lazy {
+        context?.getSharedPreferences(Const.Preference.PREFERENCE_NAME, Context.MODE_PRIVATE)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        Log.v(Const.TAG, "DeviceSearchFragment::onCreateView")
+        val contentView = inflater.inflate(R.layout.fragment_select_device, container, false)
+        contentView.findViewById<RecyclerView>(R.id.recycler_devices)?.apply {
+            layoutManager = LinearLayoutManager(view?.context)
+            adapter = this@DeviceSearchFragment.adapter
+        }
+        return contentView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.v(Const.TAG, "DeviceSearchFragment::onViewCreated")
         super.onViewCreated(view, savedInstanceState)
-    }
 
+        view.findViewById<Button>(R.id.button_choose_it).apply {
+            setOnClickListener {
+                val select = adapter.selected
+                if (select != -1) {
+                    val device = adapter.list.get(select)
+                    preference?.apply {
+                        edit().putString(Const.Preference.PREF_DEVICE, device.address).apply()
+                    }
+                }
+            }
+        }
+        view.findViewById<Button>(R.id.button_pair_now).apply {
+            setOnClickListener {
+                Toast.makeText(context, "Do pairling yourself", Toast.LENGTH_SHORT).show()  //TODO
+            }
+        }
+    }
 }
