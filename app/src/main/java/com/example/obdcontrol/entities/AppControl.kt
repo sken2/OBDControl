e1b6608baf7e101cd226ad6ea5c264bb43e88728
SHIMIZU, Kenji <sken2@yahoo.co.jp>
2020-07-09 07:58:23 +0900

    implement AppControl(cont)

---
+++ app/src/main/java/com/example/obdcontrol/entities/AppControl.kt
@@ -0,0 +1,14 @@
+package com.example.obdcontrol.entities
+
+abstract class AppControl {
+
+    abstract fun volumeUp()
+
+    abstract fun volumeDown()
+
+    abstract fun mute()
+
+    abstract fun previousTune()
+
+    abstract fun nextTune()
+}
